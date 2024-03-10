cb155ffa05ff241c54fc8e93647a9739c255a39b
Arturo_Pulgar <apulgar@correo.unimet.edu.ve>
2024-03-10 14:51:50 -0400

    probando los commits a develop

--- Paginas/HomePage.jsx
+++ Paginas/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
     return (
         <div>
             <h1>HOME</h1>
-            <Button to='/'>Volver</Button>
+            <Button id='button' to='/'>Volver</Button>
         </div>
         
     );
