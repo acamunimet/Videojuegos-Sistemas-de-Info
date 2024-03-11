0726e6e476e5043fede4313bac0de7a3a0323a40
Arturo_Pulgar <apulgar@correo.unimet.edu.ve>
2024-03-11 01:48:56 -0400

    diseño de la landing page

--- Paginas/Landing_Page.jsx
+++ Paginas/Landing_Page.jsx
@@ -1,26 +1,46 @@
+import { Link } from "react-router-dom";
 import Button from "../Componentes/Button";
-import "/Estilos/Landing_Page.css"
-import {Link} from "react-router-dom";
+import "/Estilos/Landing_Page.css";
 
 export default function Landing_Page() {
   return (
-    <div>
-      <div id="TituloLandingPage">
-        <h1>Bienvendo!!</h1>
-      </div>
+    <body>
+      <section id="seccion1_LandingPage">
+        <Link to="https://www.unimet.edu.ve/">
+          <img
+            id="LandingPage_titleImage"
+            src="https://www.unimet.edu.ve/wp-content/uploads/2023/07/Logo-footer.png"
+            alt="Description"
+          />
+        </Link>
+        <div id="LandingPage_logo">
+          <h1>GAMECLUB</h1>
 
-      <div id="seccion1_LandingPage">
-        <p>INFORMACION GENERAL DE LA PAGINA</p>
-      </div>
+          <img src="https://play-lh.googleusercontent.com/pEw9lhjwvCvhMD9h7KeGsCG11BnYo_LH8AXo2DDV_cAWIZdRtpxJS9Ds0-kmTrL-GF0" />
+        </div>
+      </section>
 
-      <div id="seccion2_LandingPage">
+      <section id="seccion2_LandingPage">
+        <h1>BIENVENIDO</h1>
+        <p>
+          <h4>INFORMACION GENERAL</h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       
+        </p>
+      </section>
+
+      <section id="seccion3_LandingPage">
         <div>
-          <Button to='/login'>Iniciar sesion</Button>
+          <Button to="/login">Iniciar sesion</Button>
         </div>
         <div>
-          <Button to='/register' >Registrarse</Button>
+          <Button to="/register">Registrarse</Button>
         </div>
-      </div>
-    </div>
+      </section>
+    </body>
   );
 }
